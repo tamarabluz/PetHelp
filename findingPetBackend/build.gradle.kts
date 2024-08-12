89dab0150edf68a86241732c22926ddc30695007
Tamara <tamarabluz@gmail.com>
2024-08-12 15:55:28 -0300

    ADM-01: Configurando a entidade

--- findingPetBackend/build.gradle.kts
+++ findingPetBackend/build.gradle.kts
@@ -27,6 +27,11 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("com.h2database:h2:2.1.214")
 }
 
 kotlin {
